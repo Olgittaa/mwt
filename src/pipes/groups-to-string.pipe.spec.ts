4fdc5589865116fc02c192226bad758724dea31c
olga_charna <pp>
2021-03-04 22:35:20 +0100

    delete spec.ts

--- src/pipes/groups-to-string.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GroupsToStringPipe } from './groups-to-string.pipe';
-
-describe('GroupsToStringPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GroupsToStringPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
